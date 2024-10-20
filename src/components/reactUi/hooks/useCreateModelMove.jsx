88e39e54b10bd6f462395f70150cb0359d1e399c
Diego Apolo <diegoapolo2011@gmail.com>
2024-10-20 05:31:21 -0700

    clean fonts and correct build dev

--- src/components/reactUi/hooks/useCreateModelMove.jsx
+++ src/components/reactUi/hooks/useCreateModelMove.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { modelByTopic } from "../../../utils";
 
 const useCreateModelMove = (topic) => {
